f5ceaae23cd71ffd138b807b0821d83e9103196c
lisbeth23 <lisbethlopez.94@gmail.com>
2022-02-13 18:16:39 -0400

    solution of question number 2

--- question-2/script.js
+++ question-2/script.js
@@ -3,6 +3,52 @@
  * This file would be executed with the following command:
  * $ node scritp.js BrowsingEvents.csv
  */
-
+const csv = require("csv-parser");
+const fs = require("fs");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const args = process.argv.slice(-1);
-console.log(`Running question #2 with args ${args}`)
+console.log(`Running question #2 with args ${args}`);
+const results = [];
+const csvWriter = createCsvWriter({
+  path: "output.csv",
+  header: [
+    { id: "productId", title: "productId" },
+    { id: "clicks", title: "clicks" },
+    { id: "impressions", title: "impressions" },
+    { id: "ctr", title: "ctr" },
+  ],
+});
+fs.createReadStream(args.toString())
+  .pipe(csv())
+  .on("data", (data) => results.push(data))
+  .on("end", () => {
+    const uniqueUsers = [...new Set(results.map((result) => result.user))];
+    const uniqueProducts = [
+      ...new Set(results.map((result) => result.entityId)),
+    ];
+    let data = [];
+    uniqueProducts.forEach((product) => {
+      let visitsPerProduct = [];
+      let clicksPerProduct = [];
+      results.forEach((result) => {
+        if (product === result.entityId && result.eventType === "click") {
+          clicksPerProduct.push(result.user);
+        }
+        if (product === result.entityId && result.eventType === "impression") {
+          visitsPerProduct.push(result.user);
+        }
+      });
+      let uniqueClicksPerProduct = [...new Set(clicksPerProduct)].length;
+      let uniqueVisitsPerProduct = [...new Set(visitsPerProduct)].length;
+      let ctr = (uniqueClicksPerProduct / uniqueVisitsPerProduct) * 100;
+      data.push({
+        productId: product,
+        clicks: uniqueClicksPerProduct,
+        impressions: uniqueVisitsPerProduct,
+        ctr: ctr,
+      });
+    });
+    csvWriter
+      .writeRecords(data)
+      .then(() => console.log("The CSV file was written successfully"));
+  });
