c5fef6a7c5d691097cf4d9beeed69a431ff65047
lisbeth23 <lisbethlopez.94@gmail.com>
2022-02-15 17:05:32 -0400

    solution of question-3 and readme

--- question-3/README.md
+++ question-3/README.md
@@ -17,3 +17,9 @@
 # Razonamiento
 
 _Escribir aquí los supuestos asumidos, reflexiones y explicaciones de la solución_
+- Inicialización de las variables: `characters` igual a la cadena entrante, `brackets` igual a una cadena con los corchetes a evaluar, `bracketsInCharacters` como un arreglo vacío, y `pile` como otro arreglo vacío.
+- Se itera `characters` como un arreglo para comparar si contiene corchetes, de ser así los almacena en `bracketsInCharacters`.
+- Se itera `bracketsInCharacters` en un ciclo for.
+- Dentro del ciclo se inicializa una variable `bracketsIndex` la cual está buscando el índice del corchete actual en la variable de `corchetes` guardada previamente.
+- Sabiendo que los corchetes de apertura tienen índices pares y los corchetes de cierre tienen índices impares se realiza la condición si `bracketsIndex` es par se agrega el `bracketsIndex + 1` a `pile` de lo contrario se elimina la adición más reciente en `pile`.
+- Al finalizar el ciclo la `pile` debe quedar vacía.
--- question-3/script.js
+++ question-3/script.js
@@ -1,23 +1,41 @@
 /**
  * On this module you should write your answer to question #3.
  * This file would be executed with the following command:
- * $ node scritp.js 'a * (b + c)'
+ * $ node script.js 'a * (b + c)'
  */
 
 const args = process.argv.slice(-1);
-console.log(`Running question #3 with args ${args}`)
+console.log(`Running question #3 with args ${args}`);
 
 /**
  * Check if a string has correct use of parenthesis.
- * 
+ *
  * @param {String} str - String to be evaluated
  * @returns {Boolean} Returns true if string is valid.
  */
 function parenthesisChecker(str) {
   // TODO: Implement validation logic
-  return true;
+    let characters = str.toString()
+    let brackets = "()[]{}"
+    let bracketsInCharacters= []
+    let pile = [] 
+    characters.split('').forEach(function(char){
+      if(brackets.includes(char)){
+        bracketsInCharacters.push(char);
+      }
+    })
+    for(let bracket of bracketsInCharacters) {
+      let bracketsIndex = brackets.indexOf(bracket)  
+      if(bracketsIndex % 2 === 0) {
+        pile.push(bracketsIndex + 1)
+      } else {
+        if(pile.pop() !== bracketsIndex) {
+          return false;
+        }
+      }
+    }
+    return pile.length === 0
 }
 
 const isValid = parenthesisChecker(args);
 console.log(`parenthesisChecker("${args}") = ${isValid}`);
-
